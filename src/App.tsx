fc9ce7aa4c2d535e6252ed8653c2bc7e1130c1fe
yhwa <global user>
2023-03-02 21:18:00 +0900

    motion value, svg animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { motion, useMotionValue, useTransform } from "framer-motion"
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion"
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 //style
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg,#e09,#d0e);
 `;
 
 const AnimationBox = styled(motion.div)`
@@ -71,6 +72,15 @@ const MotionValue = styled(motion.div)`
   background-color: white;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0,0,0,0.1), 0 10px 20px rgba(0,0,0,0.06);
+`;
+
+const Svg = styled(motion.svg)`
+width: 150px;
+
+path {
+  stroke:white;
+  stroke-width:10;
+}
 `;
 
 
@@ -144,19 +154,66 @@ const constraintDragVar = {
 function App() {
   const constraintRef = useRef<HTMLDivElement>(null);
   const x = useMotionValue(0);
-  const transformation = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  // const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(255,255,255), rgb(24,244,244), rgb(23,333,4))",
+      "linear-gradient(135deg, rgb(153, 51, 51), rgb(128, 169, 169), rgb(383,83,222))",
+      "linear-gradient(135deg, rgb(55, 158, 123), rgb(247, 190, 32), rgb(23,333,455))"
+    ]
+  );
+
+  const { scrollY, scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
+  const svg = {
+    start: {
+      pathLength: 0,
+      fill: "rgba(255,255,255,0)"
+    },
+    end: {
+      pathLength: 1,
+      fill: "rgba(255,255,255,1)",
+    }
+  }
+
+  // useEffect(() => {
+  //   scrollY.on("change", () => {
+  //     console.log(scrollY.get(), scrollYProgress.get());
+  //   });
+  // }, [scrollYProgress, scrollY]);
 
   // useEffect(() => {
   //   transformation.on("change", () => );
   // });
 
   return (
-    <Wrapper>
+    <Wrapper style={{ x, background: gradient }}>
+
+      <Svg
+        viewBox="0 0 384 512">
+        <motion.path
+          variants={svg}
+          initial="start"
+          animate="end"
+          transition={{
+            default: {
+              duration: 5
+            },
+            fill: {
+              duration: 2, delay: 1
+            }
+          }}
+          d="M290.7 311L95 269.7 86.8 309l195.7 41zm51-87L188.2 95.7l-25.5 30.8 153.5 128.3zm-31.2 39.7L129.2 179l-16.7 36.5L293.7 300zM262 32l-32 24 119.3 160.3 32-24zm20.5 328h-200v39.7h200zm39.7 80H42.7V320h-40v160h359.5V320h-40z" />
+      </Svg>
 
       <button onClick={() => x.set(200)}>click ME</button>
 
       <MotionValue
-        style={{ x, scale: transformation }}
+        style={{ x, rotateZ, scale }}
         drag="x"
         dragSnapToOrigin
       />
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createGlobalStyle } from 'styled-components';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: linear-gradient(135deg,#e09,#d0e);
-  }
-`;
-
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
